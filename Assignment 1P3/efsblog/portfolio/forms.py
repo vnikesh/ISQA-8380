2e274ac1ab5adcf6f4c9795391f6a52101d6fd59
vnikesh <nikeev1991@gmail.com>
2018-02-18 21:36:49 -0600

    Assignment 3

---
+++ Assignment 1P3/efsblog/portfolio/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Customer, Investment, Stock, Mutualfund
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ('cust_number', 'name', 'address', 'city', 'state', 'zipcode', 'email', 'cell_phone',)
+
+
+class InvestmentForm(forms.ModelForm):
+    class Meta:
+        model = Investment
+        fields = (
+            'customer', 'category', 'description', 'acquired_value', 'acquired_date', 'recent_value',
+            'recent_date',)
+
+
+class StockForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = ('customer', 'symbol', 'name', 'shares', 'purchase_price', 'purchase_date',)
+
+
+class MutualfundForm(forms.ModelForm):
+    class Meta:
+        model = Mutualfund
+        fields = (
+            'customer', 'name', 'description', 'acquired_value', 'acquired_date', 'recent_value',
+            'recent_date',)
+
---
+++ Assignment 1P3/efsblog/portfolio/models.py
@@ -0,0 +1,120 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+import requests
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    cust_number = models.IntegerField(blank=False, null=False)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=10)
+    email = models.EmailField(max_length=200)
+    cell_phone = models.CharField(max_length=50)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.cust_number)
+
+
+class Investment(models.Model):
+    customer = models.ForeignKey(Customer, related_name='investments')
+    category = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    acquired_value = models.DecimalField(max_digits=10, decimal_places=2)
+    acquired_date = models.DateField(default=timezone.now)
+    recent_value = models.DecimalField(max_digits=10, decimal_places=2)
+    recent_date = models.DateField(default=timezone.now, blank=True, null=True)
+
+    def created(self):
+        self.acquired_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.recent_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.customer)
+
+    def results_by_investment(self):
+        return self.recent_value - self.acquired_value
+
+class Stock(models.Model):
+    customer = models.ForeignKey(Customer, related_name='stocks')
+    symbol = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    shares = models.DecimalField (max_digits=10, decimal_places=1)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateField(default=timezone.now, blank=True, null=True)
+
+    def created(self):
+        self.recent_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.customer)
+
+    def initial_stock_value(self):
+        return self.shares * self.purchase_price
+
+    def current_stock_price(self):
+        symbol_f = str(self.symbol)
+        main_api = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='
+        api_key = '&interval=1min&apikey=R1FJJ5GJAGSJX5KS'
+        url = main_api + symbol_f + api_key
+        json_data = requests.get(url).json()
+        mkt_dt = (json_data["Meta Data"]["3. Last Refreshed"])
+        open_price = float(json_data["Time Series (1min)"][mkt_dt]["1. open"])
+        share_value = open_price
+        return share_value
+
+    def current_stock_value(self):
+        symbol_f = str(self.symbol)
+        main_api = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='
+        api_key = '&interval=1min&apikey=R1FJJ5GJAGSJX5KS'
+        url = main_api + symbol_f + api_key
+        json_data = requests.get(url).json()
+        mkt_dt = (json_data["Meta Data"]["3. Last Refreshed"])
+        open_price = float(json_data["Time Series (1min)"][mkt_dt]["1. open"])
+        share_value = open_price
+        return float(share_value) * float(self.shares)
+
+
+class Mutualfund(models.Model):
+    customer = models.ForeignKey(Customer, related_name='mutualfunds')
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    acquired_value = models.DecimalField(max_digits=10, decimal_places=2)
+    acquired_date = models.DateField(default=timezone.now)
+    recent_value = models.DecimalField(max_digits=10, decimal_places=2)
+    recent_date = models.DateField(default=timezone.now, blank=True, null=True)
+
+    def created(self):
+        self.acquired_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.recent_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.customer)
+
+    def results_by_mutualfund(self):
+        return self.recent_value - self.acquired_value
+
+
+# Create your models here.
